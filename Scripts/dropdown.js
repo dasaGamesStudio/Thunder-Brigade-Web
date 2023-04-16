2ab8220c061d60034872dc0c59af444e08464aac
AfeefRazick <afeefrazickamir@gmail.com>
2023-04-17 00:39:56 +0530

    af

--- Scripts/dropdown.js
+++ Scripts/dropdown.js
@@ -1,1 +1,36 @@
 // dropdown related code here
+// below is the dropdown for members
+
+const listofmembers = `
+<button class="dropbtn">Members
+  <i class="fa fa-caret-down"></i>
+</button>
+<div class="dropdown-content">
+  <a href="#">Afeef</a>
+  <a href="#">Atheek</a>
+  <a href="#">Bhanuka</a>
+  <a href="#">Nanthushan</a>
+  <a href="#">Oshada</a>
+  <a href="#">Savi</a>
+</div>
+`
+const members = document.querySelector('#members')
+members.innerHTML = listofmembers
+
+
+// below is the dropdown for members
+
+const listofprojects = `
+<button class="dropbtn">Projects
+  <i class="fa fa-caret-down"></i>
+</button>
+<div class="dropdown-content">
+  <a href="#">we</a>
+  <a href="#">have</a>
+  <a href="#">no</a>
+  <a href="#">projects</a>
+  <a href="#">currently</a>
+</div>
+`
+const projects = document.querySelector('#projects')
+projects.innerHTML = listofprojects
--- Scripts/search.js
+++ Scripts/search.js
@@ -46,38 +46,3 @@ const nav = document.querySelector('nav')
 form.innerHTML=html
 nav.append(form)
 
-// below is the dropdown for members
-
-const listofmembers = `
-<button class="dropbtn">Members
-  <i class="fa fa-caret-down"></i>
-</button>
-<div class="dropdown-content">
-  <a href="#">Afeef</a>
-  <a href="#">Atheek</a>
-  <a href="#">Bhanuka</a>
-  <a href="#">Nanthushan</a>
-  <a href="#">Oshada</a>
-  <a href="#">Savi</a>
-</div>
-`
-const members = document.querySelector('#members')
-members.innerHTML = listofmembers
-
-
-// below is the dropdown for members
-
-const listofprojects = `
-<button class="dropbtn">Projects
-  <i class="fa fa-caret-down"></i>
-</button>
-<div class="dropdown-content">
-  <a href="#">we</a>
-  <a href="#">have</a>
-  <a href="#">no</a>
-  <a href="#">projects</a>
-  <a href="#">currently</a>
-</div>
-`
-const projects = document.querySelector('#projects')
-projects.innerHTML = listofprojects
