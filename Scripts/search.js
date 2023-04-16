dc1cc398af7793e6e2ee0e7e3241e86106fae0bd
AfeefRazick <afeefrazickamir@gmail.com>
2023-04-17 00:05:25 +0530

    af dropdowns for projects and members stylised

--- Scripts/search.js
+++ Scripts/search.js
@@ -61,5 +61,23 @@ const listofmembers = `
   <a href="#">Savi</a>
 </div>
 `
-const members = document.querySelector('.dropdown')
+const members = document.querySelector('#members')
 members.innerHTML = listofmembers
+
+
+// below is the dropdown for members
+
+const listofprojects = `
+<button class="dropbtn">Projects
+  <i class="fa fa-caret-down"></i>
+</button>
+<div class="dropdown-content">
+  <a href="#">we</a>
+  <a href="#">have</a>
+  <a href="#">no</a>
+  <a href="#">projects</a>
+  <a href="#">currently</a>
+</div>
+`
+const projects = document.querySelector('#projects')
+projects.innerHTML = listofprojects
